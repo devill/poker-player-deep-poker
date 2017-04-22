b1349ffdd3ca4da209f59ce34f2d5456b59cd1e6
Istvan Szenasi <szeist@gmail.com>
2017-04-22 12:13:34 +0200

    add is_winner to game data

--- fetch_game_data.py
+++ fetch_game_data.py
@@ -10,7 +10,7 @@ games = r.json()
 game_ids = [game['id'] for game in games]
 
 # Fetch and save game data
-print('h0_rank,h0_suite,h1_rank,h1_suite,c0_rank,c0_suit,c1_rank,c1_suit,c2_rank,c2_suit,c3_rank,c3_suit,c4_rank,c4_suit')
+print('c0_rank,c0_suit,c1_rank,c1_suit,c2_rank,c2_suit,c3_rank,c3_suit,c4_rank,c4_suit,h0_rank,h0_suite,h1_rank,h1_suite,is_winner')
 
 for game_id in game_ids:
     r = requests.get(GAME_URL % game_id)
@@ -25,18 +25,21 @@ for game_id in game_ids:
 
         winner_name = re.search('(.*) won \d+', wa['message']).group(1)
 
-        winner = [player for player in game_state['players'] if player['name'] == winner_name][0]
-        cards = winner['hole_cards']
+        for player in game_state['players']:
+            if player['hole_cards']:
+                data = []
 
-        data = []
+                for community_card in community_cards:
+                    data.append(community_card['rank'])
+                    data.append(community_card['suit'])
 
-        for card in cards:
-            data.append(card['rank'])
-            data.append(card['suit'])
+                for card in player['hole_cards']:
+                    data.append(card['rank'])
+                    data.append(card['suit'])
 
-        for community_card in community_cards:
-            data.append(community_card['rank'])
-            data.append(community_card['suit'])
+                if (player['name'] == winner_name):
+                    data.append('1')
+                else:
+                    data.append('0')
 
-        print(','.join(data))
-
+                print(','.join(data))
