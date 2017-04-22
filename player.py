5a36f99a3fbc6588fa09c8f313eccd1fad02717c
DeVill <devillmeister@gmail.com>
2017-04-22 11:22:00 +0200

    Be more aggressive

--- player.py
+++ player.py
@@ -1,13 +1,10 @@
-import  json
 
 class Player:
     VERSION = "Default Python folding player"
 
 
     def get_cards_back(self,game_state):
-        
-        current_bet = 0
-        useful_ranks = ['10','J','Q','K','A']
+        useful_ranks = ['8','9','10','J','Q','K','A']
         team =  game_state['players'][game_state['in_action']]
         hole_cards_0 = team['hole_cards'][0]["rank"]
         hole_cards_1 = team['hole_cards'][1]["rank"]
@@ -21,7 +18,6 @@ class Player:
 
     def betRequest(self, game_state):
         try:
-            current_bet_in_betReq = 0
             current_bet_in_betReq = self.get_cards_back(game_state)
             current_bet_in_betReq = int(current_bet_in_betReq)
             if current_bet_in_betReq < 0:
